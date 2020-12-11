ac56525dff267361a262f7f8646444c9e9e0446e
Karla Rehren <karla.rehren@gmail.com>
2020-12-10 23:26:57 -0300

    Icono Cart y componente contenedor ItemListContainer

--- src/App.js
+++ src/App.js
@@ -1,29 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
+import ItemListContainer from './components/ItemListContainer';
 
 
-
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  return(
+      <>
+          <NavBar/>
+          <ItemListContainer 
+          saludo="Bienvenido a mi tienda"/>
+      </>
+  )
 }
 
-export default App;
+export default App
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import CartWidget from './CartWidget.js';
 
 
 
-function App() {
+function NavBar() {
     return <>
-        <Navbar bg="dark" variant="dark">
+        <Navbar bg="secondary" variant="dark">
             <Navbar.Brand href="">Kira Fightwear</Navbar.Brand>
             <Nav className="mr-auto">
             <Nav.Link href="">Inicio</Nav.Link>
             <Nav.Link href="">Jiu Jitsu Apparel</Nav.Link>
             <Nav.Link href="">Streetwear</Nav.Link>
-            <Nav.Link href="">Accesories</Nav.Link>
+            <Nav.Link href="">Accessories</Nav.Link>
             </Nav>
+            <CartWidget />
         </Navbar>
      </>;
 }
 
-export default App;
+export default NavBar;
